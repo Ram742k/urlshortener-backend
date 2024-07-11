19f0e95627aa2a9765b69b63555354f8088a4e8a
Ram742k <ramkumar742k@gmail.com>
2024-07-11 12:50:53 +0530

    activate link update

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { register, activate, login, forgotPassword, resetPassword } = require('../controllers/authController');
 
 router.post('/register', register);
-router.post('/activate/:token', activate);
+router.get('/activate/:token', activate);
 router.post('/login', login);
 router.post('/forgot-password', forgotPassword);
 router.post('/reset-password/:token', resetPassword);
